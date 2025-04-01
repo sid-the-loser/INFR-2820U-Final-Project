7635182b71566d4a07b330af8576c60cbdaa8a32
Sidharth Suresh <sidharthsuresh07@gmail.com>
2025-04-01 01:31:00 -0400

    changed the filename and fixed a couple of things more

---
+++ src/ternary search tree.py
@@ -0,0 +1,73 @@
+class Node: # Ternary Search Tree definition starts here
+	def __init__(self, char: str):
+		self.char = char # the character stored in this node
+		self.is_end_of_word = False # is this node the end of a word
+		self.left = None # left children (smaller than self.char)
+		self.middle = None # middle children (equals to self.char)
+		self.right = None # right children (greater than self.char)
+
+
+def insert(root: Node, key: str):
+	if root is None:
+		root = Node(key[0])
+
+	if key[0] < root.char:
+		root.left = insert(root.left, key)
+
+	elif key[0] > root.char:
+		root.right = insert(root.right, key)
+
+	else:
+		if len(key) > 1:
+			root.middle = insert(root.middle, key[1:])
+
+		else:
+			root.is_end_of_word = True
+
+	return root
+
+def search(root: Node, key: str) -> bool:
+	if root is None:
+		return False
+		
+	if key[0] < root.char:
+		return search(root.left, key)
+		
+	elif key[0] > root.char:
+		return search(root.right, key)
+	
+	else:
+		if len(key) > 1:
+			return search(root.middle, key[1:])
+			
+		else:
+			return root.is_end_of_word
+		
+
+def autocomplete(root: Node, key: str) -> None:
+	if root is None:
+		print("No such word found in the TST!")
+		return
+
+
+if __name__ == "__main__": # if the program is being run as its own file (i.e., not as a library or module)
+	root_node = Node("")
+ 
+	while True:
+		ch = int(input("1.Insert\n2.Search\n\nEnter your choice here (1 or 2): "))
+		print()
+ 
+		if ch == 1:
+			value = input("Enter the word to add to the autocomplete: ")
+			insert(root_node, value)
+			print(f"\"{value}\" added to autocomplete data.")
+ 
+		elif ch == 2:
+			value = input("Enter the word to autocomplete: ")
+			print(search(root_node, value))
+ 
+		else:
+			print(f"{ch} is not an option, please try 1 or 2")
+ 
+		print()
+
