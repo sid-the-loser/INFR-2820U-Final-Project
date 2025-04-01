fab63c1438c0b4e5a0897a5bbc48d9a537375122
sid-the-loser <sidharthsuresh07@gmail.com>
2025-04-01 04:18:04 -0400

    added everything fixed some bugs with the standard trie.py after a long time, ended up finishing the ternary search tree goodnight :)

--- src/standard trie.py
+++ src/standard trie.py
@@ -1,71 +1,75 @@
-class TrieNode: # Trie data structure definition starts here
+class TrieNode:  # Trie data structure definition starts here
     def __init__(self):
-        self.children = [None] * 26 # multiplied by 26 because there are 26 characters in the english alphabet
-        self.is_end_of_word = False # flag that checks if the current node is a complete word or not
+        self.children = [None] * 26  # multiplied by 26 because there are 26 characters in the english alphabet
+        self.is_end_of_word = False  # flag that checks if the current node is a complete word or not
 
-def insert(root: TrieNode, key: str) -> None: # function that is used for inserting words into trie
-    key = key.lower() # we only want the key we're inserting for to be in lowercase to avoid ASCII value issues
-                      # eg: ord("A") => 65
-                      #     ord("a") => 97
-                      #     where, `ord()` function returns the ASCII value of the character that's passed through it
 
-    node = root # this variable keeps track of the current node
+def insert(root: TrieNode, key: str) -> None:  # function that is used for inserting words into trie
+    key = key.lower()  # we only want the key we're inserting for to be in lowercase to avoid ASCII value issues
+    # eg: ord("A") => 65
+    #     ord("a") => 97
+    #     where, `ord()` function returns the ASCII value of the character that's passed through it
 
-    for char in key: # iterated through the string in key
-        i = ord(char) - 97 # 97 is the ASCII value of "a"
+    node = root  # this variable keeps track of the current node
 
-        if node.children[i] is None: # checks if that child node doesn't exist (i.e., is it null)
-            node.children[i] = TrieNode() # assigns a new node as the child to that node
+    for char in key:  # iterated through the string in key
+        i = ord(char) - 97  # 97 is the ASCII value of "a"
 
-        node = node.children[i] # moves the pointer of the trie to the next pre-existing or newly created node
+        if node.children[i] is None:  # checks if that child node doesn't exist (i.e., is it null)
+            node.children[i] = TrieNode()  # assigns a new node as the child to that node
 
-    node.is_end_of_word = True # last node that was inserted is deemed as the end of a word
+        node = node.children[i]  # moves the pointer of the trie to the next pre-existing or newly created node
 
-def autocomplete(root: TrieNode, key: str) -> None: # function to check if the word even exists in the trie, 
-													# if it does exist then using that prefix/key provided, print
-													# out the rest of the branches.
-	node = root # this variable keeps track of the current node
+    node.is_end_of_word = True  # last node that was inserted is deemed as the end of a word
 
-	for char in key: # iterated through the string in key
-		i = ord(char) - 97
 
-		if node.children[i] is None: # checks if that child node doesn't exist (i.e., is it null)
-			print("No such word found in the treap!") # prints out an error message and stops the function
-			return
+def autocomplete(root: TrieNode, key: str) -> None:  # function to check if the word even exists in the trie,
+    # if it does exist then using that prefix/key provided, print
+    # out the rest of the branches.
+    node = root  # this variable keeps track of the current node
 
-		node = node.children[i] # moves the pointer of the trie to the next pre-existing or newly created node
+    for char in key:  # iterated through the string in key
+        i = ord(char) - 97
 
-	if node.is_end_of_word: # is the node the end of the word then print a "," at the end
-		print(key, end=", ")
+        if node.children[i] is None:  # checks if that child node doesn't exist (i.e., is it null)
+            print("No such word found in the treap!")  # prints out an error message and stops the function
+            return
 
-	print_all_branches(node, key) # the function that traverses through all the given nodes children
-								  # and prints them out
+        node = node.children[i]  # moves the pointer of the trie to the next pre-existing or newly created node
 
-	print() # new line because the other function doesn't provide any new line once it's done traversing
+    if node.is_end_of_word:  # is the node the end of the word then print a "," at the end
+        print(key, end=", ")
 
-def print_all_branches(root: TrieNode, prefix: str): # printing all the children using the given prefix
-    if root.is_end_of_word: # if the node is the end of a word, then print the prefix and ","
+    print_all_branches(node, key)  # the function that traverses through all the given nodes children
+    # and prints them out
+
+    print()  # new line because the other function doesn't provide any new line once it's done traversing
+
+
+def print_all_branches(root: TrieNode, prefix: str):  # printing all the children using the given prefix
+    if root.is_end_of_word:  # if the node is the end of a word, then print the prefix and ","
         print(prefix, end=", ")
 
-    for i in range(26): # traverses through all the children of the node and recurses this function if the node exists
+    for i in range(26):  # traverses through all the children of the node and recurses this function if the node exists
         if root.children[i] is not None:
-            print_all_branches(root.children[i], prefix+chr(i+97))
+            print_all_branches(root.children[i], prefix + chr(i + 97))
 
-if __name__ == "__main__": # if the program is being run as its own file (i.e., not as a library or module)
+
+if __name__ == "__main__":  # if the program is being run as its own file (i.e., not as a library or module)
     root_node = TrieNode()
 
     while True:
-        ch = int(input("1.Insert\n2.Search\n\nEnter your choice here (1 or 2): "))
+        ch = input("1.Insert\n2.Search\n\nEnter your choice here (1 or 2): ")
         print()
 
-        if ch == 1:
+        if ch == "1":
             value = input("Enter the word to add to the autocomplete: ")
             insert(root_node, value)
             print(f"\"{value}\" added to autocomplete data.")
 
-        elif ch == 2:
+        elif ch == "2":
             value = input("Enter the word to autocomplete: ")
-            autocomplete(root_node, value) 
+            autocomplete(root_node, value)
 
         else:
             print(f"{ch} is not an option, please try 1 or 2")
--- src/ternary search tree.py
+++ src/ternary search tree.py
@@ -7,9 +7,12 @@ class Node: # Ternary Search Tree definition starts here
 		self.right = None # right children (greater than self.char)
 
 
-def insert(root: Node, key: str):
+def insert(root: Node, key: str) -> Node:
 	if root is None:
 		root = Node(key[0])
+
+	elif root.char == "":
+		root.char = key[0]
 
 	if key[0] < root.char:
 		root.left = insert(root.left, key)
@@ -26,45 +29,58 @@ def insert(root: Node, key: str):
 
 	return root
 
-def search(root: Node, key: str) -> bool:
+def exists(root: Node, key: str):
 	if root is None:
-		return False
-		
+		return None
+
 	if key[0] < root.char:
-		return search(root.left, key)
-		
+		return exists(root.left, key)
+
 	elif key[0] > root.char:
-		return search(root.right, key)
-	
+		return exists(root.right, key)
+
 	else:
 		if len(key) > 1:
-			return search(root.middle, key[1:])
-			
+			return exists(root.middle, key[1:])
+
 		else:
-			return root.is_end_of_word
-		
+			return root
+
+def print_all_branches(root: Node, prefix: str) -> None:
+	if root is None:
+		return
+
+	if root.is_end_of_word:
+		print(prefix+root.char, end=", ")
+
+	print_all_branches(root.middle, prefix+root.char)
+	print_all_branches(root.left, prefix)
+	print_all_branches(root.right, prefix)
 
 def autocomplete(root: Node, key: str) -> None:
-	if root is None:
-		print("No such word found in the TST!")
-		return
+	node = exists(root, key)
+	if node:
+		print_all_branches(node.middle, key)
+
+	else:
+		print("No such word is in the TST!")
 
 
 if __name__ == "__main__": # if the program is being run as its own file (i.e., not as a library or module)
 	root_node = Node("")
- 
+
 	while True:
-		ch = int(input("1.Insert\n2.Search\n\nEnter your choice here (1 or 2): "))
+		ch = input("1.Insert\n2.Search\n\nEnter your choice here (1 or 2): ")
 		print()
  
-		if ch == 1:
+		if ch == "1":
 			value = input("Enter the word to add to the autocomplete: ")
 			insert(root_node, value)
 			print(f"\"{value}\" added to autocomplete data.")
  
-		elif ch == 2:
+		elif ch == "2":
 			value = input("Enter the word to autocomplete: ")
-			print(search(root_node, value))
+			autocomplete(root_node, value)
  
 		else:
 			print(f"{ch} is not an option, please try 1 or 2")
